d1a1d0276327dc7eaa6d0052697cc4695330f684
Joseph-Jnr <josephnwobodo1@gmail.com>
2026-01-17 16:37:12 +0100

    feat: added play button to movie details

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,10 +1,11 @@
 import { icons } from "@/constants/icons";
 import { fetchMovieDetails } from "@/services/api";
 import { useTheme } from "@/theme/ThemeProvider";
+import { BlurView } from "expo-blur";
 import { router, useLocalSearchParams } from "expo-router";
-import { ArrowLeft } from "iconsax-react-nativejs";
-import React from "react";
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { ArrowLeft, Play, Save2 } from "iconsax-react-nativejs";
+import React, { useState } from "react";
+import { Image, Pressable, ScrollView, Text, View } from "react-native";
 import useFetch from "../../services/useFetch";
 
 interface MovieInfoProps {
@@ -38,13 +39,18 @@ const MovieInfo = ({
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
   const { background, foreground, textLight, backgroundCardBold } = useTheme();
+  const [isSaved, setIsSaved] = useState(false);
 
   const { data: movie } = useFetch(() => fetchMovieDetails(id as string));
+
+  const saveMovie = () => {
+    setIsSaved(!isSaved);
+  };
 
   return (
     <View className="flex-1" style={{ backgroundColor: background }}>
       <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
-        <View>
+        <View className="relative">
           <Image
             source={{
               uri: `https://image.tmdb.org/t/p/w500/${movie?.poster_path}`,
@@ -52,6 +58,12 @@ const MovieDetails = () => {
             className="w-full h-[550px]"
             resizeMode="stretch"
           />
+
+          <Pressable className="absolute inset-0 items-center justify-center">
+            <View className="w-18 h-18 p-2 rounded-xl bg-black/50 items-center justify-center">
+              <Play size={50} color="#fff" />
+            </View>
+          </Pressable>
         </View>
 
         <View className="flex-col items-start justify-center mt-5 px-5">
@@ -69,18 +81,44 @@ const MovieDetails = () => {
             </Text>
           </View>
 
-          <View
-            className="flex-row items-center px-2 py-1 rounded-md gap-x-1 mt-2"
-            style={{ backgroundColor: backgroundCardBold }}
-          >
-            <Image source={icons.star} className="size-4" />
-            <Text className="font-bold text-sm" style={{ color: foreground }}>
-              {Math.round(movie?.vote_average ?? 0)}/10
-            </Text>
+          <View className="flex-row items-center justify-between w-full gap-x-5">
+            <View
+              className="flex-row items-center px-2 py-1 rounded-md gap-x-1 mt-2"
+              style={{ backgroundColor: backgroundCardBold }}
+            >
+              <Image source={icons.star} className="size-4" />
+              <Text className="font-bold text-sm" style={{ color: foreground }}>
+                {Math.round(movie?.vote_average ?? 0)}/10
+              </Text>
 
-            <Text className="text-sm" style={{ color: textLight }}>
-              ({movie?.vote_count} votes)
-            </Text>
+              <Text className="text-sm" style={{ color: textLight }}>
+                ({movie?.vote_count} votes)
+              </Text>
+            </View>
+
+            <BlurView
+              intensity={20}
+              tint="systemThinMaterialLight"
+              className="rounded-2xl overflow-hidden"
+              style={{ paddingHorizontal: 16, paddingVertical: 8 }}
+            >
+              <Pressable
+                onPress={saveMovie}
+                className="flex-row items-center justify-center"
+              >
+                <Save2
+                  size={20}
+                  color={foreground}
+                  variant={isSaved ? "Bold" : "Linear"}
+                />
+                <Text
+                  className="font-semibold text-base ml-2"
+                  style={{ color: foreground }}
+                >
+                  Watch later
+                </Text>
+              </Pressable>
+            </BlurView>
           </View>
 
           <MovieInfo
@@ -126,13 +164,13 @@ const MovieDetails = () => {
         </View>
       </ScrollView>
 
-      <TouchableOpacity
+      <Pressable
         onPress={router.back}
         className="flex flex-row items-center justify-center absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-full py-3.5 z-50"
       >
         <ArrowLeft size={20} color="#fff" />
         <Text className="text-white font-semibold text-base ml-2">Go back</Text>
-      </TouchableOpacity>
+      </Pressable>
     </View>
   );
 };
